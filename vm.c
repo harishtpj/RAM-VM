250f508a686d9a61a3165685c0a2fbd7e528ad09
harishtpj <harishtpj@hotmail.com>
2022-03-24 19:13:00 +0530

    Added license to src

--- vm.c
+++ vm.c
@@ -1,4 +1,24 @@
 /* The RAM-VM Stack Based Virtual Machine */
+/* Copyright (c) 2022 Harish Kumar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 /* Includes */
 #include <stdio.h>
@@ -9,32 +29,7 @@
 #include <vm.h>
 #include <eval.h>
 
-/* Sample Program
-int program[] = {
-    PUSH, 5,
-    PUSH, 6,
-    ADD,
-    PUSH, 10,
-    MUL,
-    POP,
-    SET, RA, 99,
-    SET, RB, 9,
-    LDR, RA,
-    LDR, RB,
-    DIV,
-    POP,
-    PUSH, 78,
-    PSR, RC,
-    POP,
-    LDR, RC,
-    POP,
-    MOV, RA, RB,
-    LDR, RB,
-    POP,
-    HALT
-};
-*/
-
+/* Main Function */
 int main(int argc, char** argv) {
     if (argc <= 1) {
         printf("error: no input files\n");
